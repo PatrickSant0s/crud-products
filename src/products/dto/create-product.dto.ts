58f016f2582e393055d2575d0316de07e19fd212
Patrick Santos <132529431+PatrickSant0s@users.noreply.github.com>
2025-06-18 20:49:46 -0300

    feat: add CreateProductDto with validations

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProductDto {}
+import { IsBoolean, IsInt, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  model: string;
+
+  @IsString()
+  brand: string;
+
+  @IsString()
+  type: string;
+
+  @IsString()
+  focalLength: string;
+
+  @IsString()
+  MaxAperture: string;
+
+  @IsString()
+  mount: string;
+
+  @IsInt()
+  weight: number;
+
+  @IsBoolean()
+  haStabilization: boolean;
+
+  @IsBoolean()
+  active: boolean;
+}
